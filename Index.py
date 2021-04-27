5aa43c888be787ee138eb7bb9e43aadb515bc6a0
fouz <amirulabad@gmail.com>
2021-04-27 15:08:52 +0500

    changes

--- Index.py
+++ Index.py
@@ -3,37 +3,44 @@ cols = int(input("ENTER COLUMN NUMBERS:"))
 numOfMatrix = int(input("ENTER NUMBER OF MATRIX:"))
 
 matrices = {}
-#b = []
+# b = []
 NOM = 0
-while(NOM < numOfMatrix):
+while NOM < numOfMatrix:
     matrix = []
     n = 0
-    while (n < rows):
+    while n < rows:
         x = 0
         row = []
-        while (x < cols):
+        while x < cols:
             i = int(
-                input('ENTER NUMBER OF ROW ' + str(n + 1) + ' COLUMN ' +
-                      str(x + 1) + ':'))
+                input(
+                    "ENTER NUMBER OF ROW "
+                    + str(n + 1)
+                    + " COLUMN "
+                    + str(x + 1)
+                    + " OF MATRIX "
+                    + str(NOM + 1)
+                    + ":"
+                )
+            )
             row.append(i)
             x = x + 1
         matrix.append(row)
         n = n + 1
     # print(matrix)
-    matrices['Matrix '+str(NOM+1)] = matrix
+    matrices["Matrix " + str(NOM + 1)] = matrix
     NOM = NOM + 1
-#print(l)
-
+# print(l)
 
 
 k = 0
-while (k < len(matrices)):
-    print("Matrix " + str(k+1))
-    m = (matrices['Matrix '+ str(k+1)])
+while k < len(matrices):
+    print("Matrix " + str(k + 1))
+    m = matrices["Matrix " + str(k + 1)]
     p = 0
-    while (p < len(m)):
+    while p < len(m):
         print(m[p])
         p = p + 1
     k = k + 1
 
-#MADE BY ASAD AND FOUZ AND FAIQ AND ASDULLAH AND AMAN AND QAMBER ALI4
+# MADE BY ASAD AND FOUZ AND FAIQ AND ASDULLAH AND AMAN AND QAMBER ALI4
